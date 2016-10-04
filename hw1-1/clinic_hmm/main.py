5b5a7dae7ea03508f31472c9d9a8a9c1e4f4b64f
AdrianHsu <adrianhsu1995@gmail.com>
2016-10-04 14:54:08 +0800

    viterbi alg read

--- hw1-1/clinic_hmm/main.py
+++ hw1-1/clinic_hmm/main.py
@@ -3,15 +3,24 @@
 
 # "隱含"state的list （S，代表現在healthy or sick。注意hmm的state都是"hidden"，因為只能從observations中推斷）
 states = ('Healthy', 'Fever')
-# observation的種類（V，HMM才有這個）
+
+# 測資，丟進去的observation序列（V，HMM才有這個）
 observations = ('normal', 'cold', 'dizzy')
+# 結果：(0.01512, ['Healthy', 'Healthy', 'Fever']) 
+observations_2 = ('normal', 'cold', 'normal')
+# 結果：(0.0294, ['Healthy', 'Healthy', 'Healthy'])
+observations_3 = ('dizzy', 'cold', 'normal')
+# 結果：(0.01344, ['Fever', 'Healthy', 'Healthy'])
+
 # 初始化（pi節點）的機率 （一個人在今天算起是健康or生病？所以特別把邊界條件抓出來）
 start_probability = {'Healthy': 0.6, 'Fever': 0.4}
+
 # 轉態機率（每條邊aij的條件機率）現在在state = 健康，下次state = 健康的機率為0.7
 transition_probability = { 
    'Healthy' : {'Healthy': 0.7, 'Fever': 0.3},
    'Fever' : {'Healthy': 0.4, 'Fever': 0.6},
    }
+
 # 噴出這種observation的條件機率。現在在state = 健康，當下噴出的observation = dizzy的機率為0.1
 # 表示每天病人感覺的可能性。假如他是健康的，50%會感覺正常。如果他發燒了，有60%的可能感覺到頭暈。
 emission_probability = {
@@ -33,32 +42,32 @@ def print_dptable(V):
 
 # 看到一個觀察序列 o 1 o 2 ...... o T ，
 # 但是看不到狀態序列 s 1 s 2 ...... s T 的情況下，
-# 從所有可能的路徑當中，找出機率最大的一條路徑(e.g. (0.01512, ['Healthy', 'Healthy', 'Fever']) )，以及其機率。
+# 從所有可能的路徑當中，找出機率最大的一條路徑，以及其機率。e.g.結果：(0.01512, ['Healthy', 'Healthy', 'Fever']) 
 def viterbi(obs, states, start_p, trans_p, emit_p):
-    V = [{}]
-    path = {}
+	V = [{}]
+	path = {}
 
-    # Initialize base cases (t == 0)
-    for y in states:
-        V[0][y] = start_p[y] * emit_p[y][obs[0]]
-        path[y] = [y]
+	# 初始化base cases (時間t = 0)
+	for y in states:
+		V[ 0 ][ y ] = start_p[ y ] * emit_p[y][obs[0]]
+		path[y] = [y]
+	
+	# 跑 Viterbi alog. for時間 t > 0
+	for t in range(1, len(obs)):
+		V.append({})
+		newpath = {}
 
-    # Run Viterbi for t > 0
-    for t in range(1,len(obs)):
-        V.append({})
-        newpath = {}
+		for y in states:
+			(prob, state) = max([(V[t - 1][ y0 ] * trans_p[ y0 ][ y ] * emit_p[ y ][ obs[t] ], y0) for y0 in states])
+			V[t][y] = prob
+			newpath[y] = path[state] + [y]
 
-        for y in states:
-            (prob, state) = max([(V[t-1][y0] * trans_p[y0][y] * emit_p[y][obs[t]], y0) for y0 in states])
-            V[t][y] = prob
-            newpath[y] = path[state] + [y]
-
-        # Don't need to remember the old paths
+        # 不需要記得剛剛走哪一條線（不用管上一筆轉態機率）
         path = newpath
 
-    print_dptable(V)
-    (prob, state) = max([(V[len(obs) - 1][y], y) for y in states])
-    return (prob, path[state])
+	print_dptable(V)
+	(prob, state) = max( [( V[len( obs ) - 1][ y ], y) for y in states] )
+	return (prob, path[state])
 
 
 def main():
