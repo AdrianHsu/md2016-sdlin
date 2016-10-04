0529968f86671837cf96ea595abcf61226c8c186
AdrianHsu <adrianhsu1995@gmail.com>
2016-10-04 23:41:27 +0800

    done hw1-1

--- hw1-1/main.py
+++ hw1-1/main.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 #-*- coding: UTF-8 -*- 
 
+import viterbi
 import numpy as np
 import sys
 
 pi = np.zeros((1, 37))
-f = np.zeros((37, 37))
-p = np.zeros((37, 37))
+for i in range(0, len(pi[0])):
+   pi[0][i] = (1.0 / 37.0)
+
+trans = np.zeros((37, 37))
+obs = np.zeros((37, 37))
 
 # Bigram language model
 def bigram():
@@ -17,18 +21,16 @@ def bigram():
       argv = _line.split(' ')
       a = _line[0]
       b = _line[2]
-      f = 0.0
+      func = 0.0
       if len(_line) > 5:
          try:
             _str = argv[len(argv) - 1]
-            f = float(_str)
-         except ValueError:
-            f = 0.0
+            func = float(_str)
+         except ValueError: #impossible
+            func = 0.0
       else:
-         f = 0.0
-      #print ord(a), 
-      #print ord(b),
-      #print f
+         func = 0.0
+      trans[ index(a) ][ index(b) ] = func
    _file.close
 
 # Encoding Table
@@ -40,39 +42,44 @@ def encoding():
       argv = _line.split(' ')
       x = _line[0]
       y = _line[2]
-      p = 0.0
+      prob = 0.0
       if len(_line) > 5:
          try:
             _str = argv[len(argv) - 1]
-            p = float(_str)
-         except ValueError:
-            p = 0.0
+            prob = float(_str)
+         except ValueError: #impossible
+            prob = 0.0
       else:
-         p = 0.0
-      #print ord(x),
-      #print ord(y),
-      #print p
+         prob = 0.0
+      obs[ index(x) ][ index(y) ] = prob
 
    _file.close
 
-def test():
-   _file = open('tmp.txt', 'r')
+def test(d):
+   _file = open('test.txt', 'r')
    _text = _file.read()
    
-   _words = _text.split(' ')
+   _words = _text.split()
    _word_num = len(_words)
-   
+
    for _word in _words:
       length = len(_word)
-      test_str(_word, length)
+      test_str(d, _word, length)
 
    _file.close
 
-def test_str(_word, length):
-   arr = np.zeros((1, length))
+def test_str(d, _word, length):
+   _arr = [-1] * length
    for i in range(0, length):
-      arr[0][i] = index(_word[i])
-      print arr[0][i]
+      _arr[i] = index(_word[i])
+      #print _arr[i]
+   print_str(d.Decode(_arr), length)
+
+def print_str(_arr, length):
+   str = ""
+   for i in range(0,length):
+      str += char(_arr[i])
+   print str,
 
 def index(_ch):
    if ord(_ch) >= 48 and ord(_ch) <= 57: #index('9') = 9
@@ -99,7 +106,10 @@ def char(_index):
 def main():
    bigram()
    encoding()
-   test()
+   d = viterbi.Decoder(pi.T, trans, obs)
+
+   test(d)
+   
    #print ord('a'), #97, chr(97) = 'a'
    #print ord('z'), #122
    #print ord('0'), #48
