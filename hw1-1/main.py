f24b1947be3fbc8bc37ea45069ec8c72f49949ae
AdrianHsu <adrianhsu1995@gmail.com>
2016-10-04 17:24:47 +0800

    index, cha

--- hw1-1/main.py
+++ hw1-1/main.py
@@ -21,7 +21,8 @@ def bigram():
             f = 0.0
       else:
          f = 0.0
-      print a + " " + b,
+      print ord(a), 
+      print ord(b),
       print f
    _file.close
 
@@ -43,15 +44,46 @@ def encoding():
             p = 0.0
       else:
          p = 0.0
-      print x + " " + y,
+      print ord(x),
+      print ord(y),
       print p
 
    _file.close
+
+def index(_ch):
+   if ord(_ch) >= 48 and ord(_ch) <= 57: #index('9') = 9
+      return ord(_ch) - 48 
+   elif ord(_ch) >= 97 and ord(_ch) <= 122: # index('a') = 10 
+      return ord(_ch) - 87
+   else:
+      return 0
+
+def char(_index):
+   if _index >= 0 and _index <= 9:
+      return chr(_index + 48)
+   elif _index >= 10 and _index <= 35:
+      return chr(_index + 87)
+   else:
+      return '-'
+
 
 # Main function
 def main():
    bigram()
    encoding()
+   #print ord('a'), #97, chr(97) = 'a'
+   #print ord('z'), #122
+   #print ord('0'), #48
+   #print ord('9')  #57
+   #print index('0'), # = 0
+   #print index('9'), # = 9
+   #print index('a'), # = 10
+   #print index('z')  # = 35
+   #print char(0),
+   #print char(9),
+   #print char(10),
+   #print char(35)
+
 
 if __name__ == '__main__':
    main()
